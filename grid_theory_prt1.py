13dbf196a7287e240a6c2aa45c3b159cd3205315
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-04-22 18:43:46 +0300

    Update grid_theory_prt1.py

--- grid_theory_prt1.py
+++ grid_theory_prt1.py
@@ -47,31 +47,3 @@ for row, col in route[::-1]:
 
 for line in output:
     print(''.join(line))
-import random
-
-def generate_grid(n, start_row, start_col, end_row, end_col):
-    grid = [['.' for _ in range(n)] for _ in range(n)]
-    grid[start_row][start_col] = 'S'
-    grid[end_row][end_col] = 'T'
-    path = []
-    current_row, current_col = start_row, start_col
-    while current_row != end_row or current_col != end_col:
-        if current_row < end_row:
-            current_row += 1
-        elif current_row > end_row:
-            current_row -= 1
-        if current_col < end_col:
-            current_col += 1
-        elif current_col > end_col:
-            current_col -= 1
-        path.append((current_row, current_col))
-
-    wall_count = 0
-    while wall_count < n * n // 4: 
-        row = random.randint(0, n - 1)
-        col = random.randint(0, n - 1)
-        if (row, col) not in path and (row, col) != (start_row, start_col) and (row, col) != (end_row, end_col):
-            grid[row][col] = '#'
-            wall_count += 1
-
-    return grid
